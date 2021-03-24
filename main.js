7bd666341ac044ddab09e48e14a67ed20f674f59
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-03-24 00:05:40 -0700

    added painting func

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("container");
+const colorPicker = document.getElementById("color-picker");
 const gameContainer = document.getElementById("game-container");
 const accelerateBtn = document.getElementById("accelerate-btn");
 const paintCanvas = document.getElementById("paintcanvas");
@@ -9,6 +10,7 @@ let defaultGamePiece;
 let isDrawing = false;
 let mouseX = 0;
 let mouseY = 0;
+let drawnArray = [];
 
 // const startGame = () => {
 //     defaultGamePiece = new component(30, 30, "blue", 80, 80);
@@ -36,8 +38,8 @@ let mouseY = 0;
 
 const drawSquare = (x, y, width) => {
     paintContext.beginPath();
-    paintContext.strokeStyle = "gray";
-    paintContext.lineWitdh = 1;
+    paintContext.strokeStyle = "lightgray";
+    paintContext.lineWitdh = 0.1;
     paintContext.moveTo(x, y);
     paintContext.lineTo(x + width, y);
     paintContext.lineTo(x + width, y + width);
@@ -54,7 +56,7 @@ const makeGrid = (width, height) => {
     for (let x = 0; x <= width; x = x + 10) {
             for (let y = 0; y <= height; y = y + 10) {
                 drawSquare(x, y, 10);
-                console.log(`will draw square, x: ${x}, y: ${y}`);     
+                // console.log(`will draw square, x: ${x}, y: ${y}`);     
                 countY ++;
             }
             countX ++;
@@ -76,24 +78,42 @@ const makeGrid = (width, height) => {
     //     }
     //     // paintContext.closePath();
 
-
     paintCanvas.addEventListener("mousedown", e => {
         console.log("mouse is down");
         isDrawing = true;
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+        paintContext.beginPath();
+        // paintContext.strokeStyle = colorPicker.value;
+        // paintContext.lineWitdh = 5;
+        // paintContext.moveTo(mouseX, mouseY);
+        // console.log(mouseX, mouseY);
+        // console.log(colorPicker.value);
     });
 
     paintCanvas.addEventListener("mousemove", e => {
         if (isDrawing === true) {
-            // console.log("you are drawing");
-            mouseX = e.offsetX;
-            mouseY = e.offsetY;
-        }
-        //  console.log(event.offsetX, event.offsetY);
+            console.log("you are drawing");
+            // mouseX = e.offsetX;
+            // mouseY = e.offsetY;
+            // console.log(`xx: ${e.offsetX}, yy: ${e.offsetY}`);
+            paintContext.strokeStyle = colorPicker.value;
+            paintContext.lineWidth = 5;
+            paintContext.lineTo(e.offsetX, e.offsetY);
+            paintContext.stroke();
+            // paintContext.moveTo(mouseX, mouseY);
+            console.log(mouseX, mouseY);
+            console.log(colorPicker.value);
+            console.log(`x: ${event.offsetX}, y: ${event.offsetY}`);
+        } else {
+            return;
+        }    
     });
 
     paintCanvas.addEventListener("mouseup", e => {
         // console.log("mouse is up");
         isDrawing = false;
+        paintContext.beginPath();
         drawSquare(mouseX, mouseY, 20);
     });
  }
